d2bfa76bc826a49fab85e840f0dd29f20ecfbc2c
Luis Milla <anthony.milla1997@gmail.com>
2022-09-13 20:33:05 -0500

    Añadido lab 5.1 y 5.2

--- lab5/lab51.php
+++ lab5/lab51.php
@@ -1,31 +1,20 @@
 <?php
-    
-    if($_FILES['nombre_archivo_cliente']['type'] == 'image/jpeg' || $_FILES['nombre_archivo_cliente']['type'] == 'image/png' 
-    || $_FILES['nombre_archivo_cliente']['type'] == 'image/gif') {
-        
-        if($_FILES['nombre_archivo_cliente']['size'] < 1048576) {
-            if (is_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'])) {
+    if (is_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'])) {
             
-                $nombreDirectorio = "archivos/";
-                $nombrearchivo = $_FILES['nombre_archivo_cliente']['name'];
-                $nombreCompleto = $nombreDirectorio . $nombrearchivo;
+        $nombreDirectorio = "archivos/";
+        $nombrearchivo = $_FILES['nombre_archivo_cliente']['name'];
+        $nombreCompleto = $nombreDirectorio . $nombrearchivo;
         
-                if (is_file($nombreCompleto)) {
-                    $idUnico = time();
-                    $nombrearchivo = $idUnico . "-" . $nombrearchivo;
-                    echo "Archivo repetido, se cambiara el nombre a $nombrearchivo 
-                    <br><br>";
-                }
-                move_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'],
-                $nombreDirectorio . $nombrearchivo);
-                echo "El archivo se ha subido satisfactoriamente al directorio $nombreDirectorio<br>";
-            } else {
-                echo "No se ha podido subir el archivo <br>";
-            }
-        } else {
-            echo "El archivo debe ser inferior a 1 MB <br>";
+        if (is_file($nombreCompleto)) {
+            $idUnico = time();
+            $nombrearchivo = $idUnico . "-" . $nombrearchivo;
+            echo "Archivo repetido, se cambiara el nombre a $nombrearchivo 
+            <br><br>";
         }
-    } else {
-        echo "El archivo solo puede ser de tipo: jpg, jpeg, png o gif <br>";
-    }
+        move_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'],
+        $nombreDirectorio . $nombrearchivo);
+        echo "El archivo se ha subido satisfactoriamente al directorio $nombreDirectorio<br>";
+        } else {
+            echo "No se ha podido subir el archivo <br>";
+        }
 ?>
